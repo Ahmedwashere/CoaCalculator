e6a3ca0ca7693b9fab57824a8305a9e8e888ff80
ahmedwashere <aeldeso@xula.edu>
2024-09-17 03:05:50 -0500

    Added Radio Buttons for the semesters of UT Austin

--- app/src/main/java/com/example/coacalculator/MainActivity.kt
+++ app/src/main/java/com/example/coacalculator/MainActivity.kt
@@ -12,10 +12,15 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.selection.selectableGroup
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -26,6 +31,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -48,9 +54,57 @@ class MainActivity : ComponentActivity() {
             Column {
                 Header()
                 UsersFullNameInputBoxAndOutput()
+                SemesterSelector()
             }
         }
     }
+}
+
+@Composable
+fun SemesterSelector() {
+    val semesters = listOf("Both Fall and Spring", "Fall", "Spring", "Summer")
+    val (selectedSemester, onSemesterSelected) = remember { mutableStateOf(semesters[0]) }
+
+    Column(Modifier.padding(16.dp)) {
+        Text(text = "Semesters", fontSize = 24.sp, fontWeight = FontWeight.Medium)
+        Column(modifier = Modifier.selectableGroup()) {
+            semesters.forEach { text ->
+                Card(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(4.dp),
+                    shape = RoundedCornerShape(0.dp),
+                    )
+                 {
+                    Row(
+                        Modifier
+                            .fillMaxWidth()
+                            .height(45.dp)
+                            .selectable(
+                                selected = (text == selectedSemester),
+                                onClick = { onSemesterSelected(text) },
+                                role = Role.RadioButton
+                            )
+                            .padding(horizontal = 16.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        RadioButton(selected = (text == selectedSemester), onClick = null)
+                        Text(
+                            text = text, modifier = Modifier.padding(start = 16.dp),
+                            style = MaterialTheme.typography.bodyLarge,
+                            fontWeight = FontWeight.SemiBold
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SemesterSelectorPreview() {
+    SemesterSelector()
 }
 
 @Composable
@@ -74,11 +128,12 @@ fun UsersFullNameInputBoxAndOutput() {
                 label = { Text("Full Name") },
                 modifier = Modifier.padding(8.dp),
             )
-            Button(onClick = {
-                dynamicTextOutput =
-                    if (text.isNotBlank()) "Welcome to the XULA cost of attendance calculator $text"
-                    else "Please provide text as input."
-            },
+            Button(
+                onClick = {
+                    dynamicTextOutput =
+                        if (text.isNotBlank()) "Welcome to the XULA cost of attendance calculator $text"
+                        else "Please provide text as input."
+                },
                 shape = RoundedCornerShape(10.dp),
             ) {
                 Text("Submit")
@@ -94,9 +149,9 @@ fun UsersFullNameInputBoxAndOutput() {
     }
 }
 
-@Preview
+@Preview(showBackground = true)
 @Composable
-fun UserFullNameInputBoxAndOutputPreview(){
+fun UserFullNameInputBoxAndOutputPreview() {
     UsersFullNameInputBoxAndOutput()
 }
 
